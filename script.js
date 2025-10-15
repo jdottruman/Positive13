161d1f78d9a377dd9b0826732dd978007d903b56
jdottruman <jadyntruman2@gmail.com>
2025-10-14 19:18:34 -0500

    with cards

--- script.js
+++ script.js
@@ -26,16 +26,8 @@ const card = [
 let cardHtml =``;
 card.forEach((info,index)=>{
 cardHtml+= `
-<div class="card">
-<div class="cardImageContainer">
-    <img src="${info.photo}" alt="">
-</div>
-<div class="card-details">
-    <p class="text-title">${info.title}</p>
-    <p class="text-body">${info.description}</p>
-</div>
-<button class="card-button">Learn More</button>
-</div>
+<div class="card"></div>
+
 
 `
 });
