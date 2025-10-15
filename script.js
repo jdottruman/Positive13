91e3395ba43b7a2cca59e1fad6e4a5e94f4aa295
jdottruman <jadyntruman2@gmail.com>
2025-10-14 20:01:45 -0500

    new cards

--- script.js
+++ script.js
@@ -26,7 +26,16 @@ const card = [
 let cardHtml =``;
 card.forEach((info,index)=>{
 cardHtml+= `
-<div class="card"></div>
+
+<div class="card">
+    <div class="content">
+    <p class="logo">Digital Way</p>
+      <div class="h6">Data &amp; Infrastructure Agility</div>
+      <div class="hover_content">
+        <p>mParticle’s customer data platform empowers you to Integrate all of your data and orchestrate it across channels, partners, and systems.</p>
+      </div>
+    </div>
+  </div>       
 
 
 `
